c92644930434007a51b23dd3a673d29472a904da
UnknownGuyLoo <mujtabashaikh2007@gmail.com>
2025-04-30 12:17:22 +0530

    last save before reinstall

--- hyprland/.config/hypr/Scripts/setwall.sh
+++ hyprland/.config/hypr/Scripts/setwall.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 effects=("grow" "wave" "any" "fade")
-random_index=$(( RANDOM % ${#effects[@]} )) 
+random_index=$((RANDOM % ${#effects[@]}))
 img=$(sxiv -to ~/.dotfiles/wallpapers/ | awk -F'/' '{print $NF}')
 swww img -t ${effects[random_index]} ~/.dotfiles/wallpapers/$img
-wallust run ~/.dotfiles/wallpapers/$img
